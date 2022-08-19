d2bcc6d0e3410f3791c2ab7fe5a1a4d7b00d60c6
ErikPC <epardillo@cifpfbmoll.eu>
2022-08-19 10:57:48 +0200

    feat: create project

---
+++ fit_rest_full/src/test/java/edu/poniperro/FitnessResourceIT.java
@@ -0,0 +1,8 @@
+package edu.poniperro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FitnessResourceIT extends FitnessResourceTest {
+    // Execute the same tests but in packaged mode.
+}
